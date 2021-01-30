db80d8d5f55b13c791074b60bc6804390223e143
imsisi44 <74580659+imsisi44@users.noreply.github.com>
2021-01-30 21:28:10 +0900

    commit

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
+  Payjp.setPublicKey('pk_test_7f7ca102cf78cb722e9bbc3e');
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
