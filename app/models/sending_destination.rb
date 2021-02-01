250865fdaf9d0ab88cfdccd4c4e04c9ccb38f931
imsisi44 <74580659+imsisi44@users.noreply.github.com>
2021-02-01 22:53:25 +0900

    c

--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -8,7 +8,7 @@ class SendingDestination < ApplicationRecord
   validates :house_number, presence: true
   validates :phone_number, presence: true
 
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :city
   belongs_to :item
 
